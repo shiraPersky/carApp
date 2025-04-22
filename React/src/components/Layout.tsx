d09f4b8104866d28d9b65b12b7e2ba514acd746c
Shira <shira.persky@gmail.com>
2025-04-22 11:22:03 +0300

    remove footer from cars

--- React/src/components/Layout.tsx
+++ React/src/components/Layout.tsx
@@ -1,4 +1,25 @@
+// import React from 'react';
+// import FooterNav from './FooterNav';
+
+// interface LayoutProps {
+//   children: React.ReactNode;
+// }
+
+// const Layout = ({ children }: LayoutProps) => {
+//   return (
+//     <div className="layout">
+//       <div className="content">
+//         {children}
+//       </div>
+//       <FooterNav />
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import FooterNav from './FooterNav';
 
 interface LayoutProps {
@@ -6,12 +27,18 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+
+  // Hide footer on specific routes
+  const hideFooterRoutes = ['/cars'];
+  const shouldShowFooter = !hideFooterRoutes.includes(location.pathname);
+
   return (
     <div className="layout">
       <div className="content">
         {children}
       </div>
-      <FooterNav />
+      {shouldShowFooter && <FooterNav />}
     </div>
   );
 };
